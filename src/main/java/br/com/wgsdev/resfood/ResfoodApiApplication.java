ea61d5d4cdf4b68bef5603a7f329aea905e33e29
Wislan Santos <wislansantosdev@gmail.com>
2026-04-29 12:21:00 -0300

    first commit #1

---
+++ src/main/java/br/com/wgsdev/resfood/ResfoodApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.wgsdev.resfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResfoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wgsdev/resfood/ResfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wgsdev.resfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class ResfoodApiApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
