1de8b0cee103d51a2017865ef79a9d1cdb4e27ab
Wislan Santos <wislansantosdev@gmail.com>
2026-05-06 19:10:09 -0300

    adicionar excecoes customizadas #5

---
+++ src/main/java/br/com/wgsdev/resfood/domain/exception/EntidadeEmUsoException.java
@@ -0,0 +1,11 @@
+package br.com.wgsdev.resfood.domain.exception;
+
+public class EntidadeEmUsoException extends RuntimeException {
+    
+    private static final long serialVersionUID = 1L;
+    
+    public EntidadeEmUsoException(String mensagem) {
+        super(mensagem);
+    }
+    
+}
---
+++ src/main/java/br/com/wgsdev/resfood/domain/exception/EntidadeNaoEncontradaException.java
@@ -0,0 +1,11 @@
+package br.com.wgsdev.resfood.domain.exception;
+
+public class EntidadeNaoEncontradaException extends RuntimeException {
+    
+    private static final long serialVersionUID = 1L;
+    
+    public EntidadeNaoEncontradaException(String mensagem) {
+        super(mensagem);
+    }
+    
+}
