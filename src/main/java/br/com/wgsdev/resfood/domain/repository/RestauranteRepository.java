22864e14c481e986a4abe8a9605b916126f5d9cc
Wislan Santos <wislansantosdev@gmail.com>
2026-05-20 10:57:05 -0300

    resolver problema n+1 em restaurante #7

--- src/main/java/br/com/wgsdev/resfood/domain/repository/RestauranteRepository.java
+++ src/main/java/br/com/wgsdev/resfood/domain/repository/RestauranteRepository.java
@@ -8,4 +8,7 @@ import br.com.wgsdev.resfood.domain.model.Restaurante;
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
     
+    @Query("from Restaurante r join fetch r.cozinha left join fetch r.formasPagamento")
+    List<Restaurante> findAll();
+    
 }
