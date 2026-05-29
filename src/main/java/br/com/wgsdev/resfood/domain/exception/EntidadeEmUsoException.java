fe3fc5c6fc4f7ee4e018178e431885591a70909f
Wislan Santos <wislansantosdev@gmail.com>
2026-05-29 15:40:24 -0300

    adicionar anotacao response status nas classes de excecao #9

--- src/main/java/br/com/wgsdev/resfood/domain/exception/EntidadeEmUsoException.java
+++ src/main/java/br/com/wgsdev/resfood/domain/exception/EntidadeEmUsoException.java
@@ -1,5 +1,9 @@
 package br.com.wgsdev.resfood.domain.exception;
 
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class EntidadeEmUsoException extends RuntimeException {
     
     private static final long serialVersionUID = 1L;
