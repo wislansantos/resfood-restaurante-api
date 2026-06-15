6c7808c557d1e15cf58e8ab3a85ab14bc8d1ef9f
Wislan Santos <wislansantosdev@gmail.com>
2026-06-15 13:57:19 -0300

    tratar outras excecoes nao capturadas (#9)

--- src/main/java/br/com/wgsdev/resfood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/br/com/wgsdev/resfood/api/exceptionhandler/ApiExceptionHandler.java
@@ -29,7 +29,22 @@ import br.com.wgsdev.resfood.api.exceptionhandler.ProblemType;
 @RestControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     
-	@Override
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<Object> handleUncaught(Exception ex, WebRequest request) {
+		HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;		
+		ProblemType problemType = ProblemType.ERRO_DE_SISTEMA;
+		String detail = "Ocorreu um erro interno inesperado no sistema. "
+				+ "Tente novamente e se o problema persistir, entre em contato "
+				+ "com o administrador do sistema.";
+	    
+		ex.printStackTrace();
+		
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+	}
+    
+    @Override
     protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         ProblemType problemType = ProblemType.RECURSO_NAO_ENCONTRADO;
         String detail = String.format("O recurso '%s', que você tentou acessar, é inexistente.", ex.getRequestURL());
