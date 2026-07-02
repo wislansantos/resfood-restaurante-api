4b8d9857670cebc2630472c9472258e49cdbb271
Wislan <160436045+wislansantos@users.noreply.github.com>
2026-07-02 11:26:02 +0000

    descomentar testes

--- src/test/java/br/com/wgsdev/resfood/ResfoodApiApplicationTests.java
+++ src/test/java/br/com/wgsdev/resfood/ResfoodApiApplicationTests.java
@@ -3,10 +3,10 @@ package br.com.wgsdev.resfood;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class ResfoodApiApplicationTests {
 
-	//@Test
+	@Test
 	void contextLoads() {
 	}
 
