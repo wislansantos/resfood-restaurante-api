b0edf2707c25f83e044b1e766973fef3a8170443
Wislan <160436045+wislansantos@users.noreply.github.com>
2026-07-05 19:28:54 +0000

    Aprender o primeiro teste e fazer ele falhar (#11)

--- src/test/java/br/com/wgsdev/resfood/ResfoodApiApplicationTests.java
+++ src/test/java/br/com/wgsdev/resfood/ResfoodApiApplicationTests.java
@@ -1,13 +1,16 @@
 package br.com.wgsdev.resfood;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ResfoodApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+    assertFalse(true);
+  }
 
 }
