f0ee94ae7025932ae1b38a55d4ab87273a1e9420
Wislan <160436045+wislansantos@users.noreply.github.com>
2026-07-07 20:02:48 +0000

    Consertar erro nos testes (#11)

--- src/test/java/br/com/wgsdev/resfood/CadastroCozinhaIntegrationTests.java
+++ src/test/java/br/com/wgsdev/resfood/CadastroCozinhaIntegrationTests.java
@@ -1,5 +1,7 @@
 package br.com.wgsdev.resfood;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,36 +19,42 @@ import br.com.wgsdev.resfood.domain.exception.CozinhaNaoEncontradaException;
 @SpringBootTest
 class CadastroCozinhaIntegrationTests {
 
-    @Autowired
-    private CadastroCozinhaService cadastroCozinha;
+  @Autowired
+  private CadastroCozinhaService cadastroCozinha;
 
-    @Test
-    public void deveAtribuirId_QuandoCadastrarCozinhaComDadosCorretos() {
-        Cozinha novaCozinha = new Cozinha();
-        novaCozinha.setNome("Chinesa");
+  @Test
+  public void deveAtribuirId_QuandoCadastrarCozinhaComDadosCorretos() {
+    Cozinha novaCozinha = new Cozinha();
+    novaCozinha.setNome("Chinesa");
 
-        novaCozinha = cadastroCozinha.salvar(novaCozinha);
+    novaCozinha = cadastroCozinha.salvar(novaCozinha);
 
-        assertThat(novaCozinha).isNotNull();
-        assertThat(novaCozinha.getId()).isNotNull();
-    }
+    assertThat(novaCozinha).isNotNull();
+    assertThat(novaCozinha.getId()).isNotNull();
+  }
 
-    @Test(expected = ConstraintViolationException.class)
-	public void deveFalhar_QuandoCadastrarCozinhaSemNome() {
-		Cozinha novaCozinha = new Cozinha();
-		novaCozinha.setNome(null);
-		
-		novaCozinha = cadastroCozinha.salvar(novaCozinha);
-	}
-    
-    @Test(expected = EntidadeEmUsoException.class)
-	public void deveFalhar_QuandoExcluirCozinhaEmUso() {
-		cadastroCozinha.excluir(1L);
-	}
+  @Test
+  public void deveFalhar_QuandoCadastrarCozinhaSemNome() {
+    Cozinha novaCozinha = new Cozinha();
+    novaCozinha.setNome(null);
 
-	@Test(expected = CozinhaNaoEncontradaException.class)
-	public void deveFalhar_QuandoExcluirCozinhaInexistente() {
-		cadastroCozinha.excluir(100L);
-	}
+    assertThrows(ConstraintViolationException.class, () -> {
+      cadastroCozinha.salvar(novaCozinha);
+    });
+  }
+
+  @Test
+  public void deveFalhar_QuandoExcluirCozinhaEmUso() {
+    assertThrows(EntidadeEmUsoException.class, () -> {
+      cadastroCozinha.excluir(1L);
+    });
+  }
+
+  @Test
+  public void deveFalhar_QuandoExcluirCozinhaInexistente() {
+    assertThrows(CozinhaNaoEncontradaException.class, () -> {
+      cadastroCozinha.excluir(100L);
+    });
+  }
 
 }
