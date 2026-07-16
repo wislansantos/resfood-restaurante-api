1045ee1532b93ba5212e7a1d10ae144e23f37317
Wislan Santos <wislansantosdev@gmail.com>
2026-07-16 20:33:15 -0300

    Voltar o estado inicial do banco de dados para cada execução de teste com callback do Flyway (#11)

--- src/test/java/br/com/wgsdev/resfood/CadastroCozinhaIT.java
+++ src/test/java/br/com/wgsdev/resfood/CadastroCozinhaIT.java
@@ -1,8 +1,10 @@
 package br.com.wgsdev.resfood;
 
+import org.flywaydb.core.Flyway;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -16,11 +18,16 @@ class CadastroCozinhaIT {
   @LocalServerPort
   private int port;
 
+  @Autowired
+  private Flyway flyway;
+
   @BeforeEach
   public void setUp() {
     RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
     RestAssured.port = port;
     RestAssured.basePath = "/cozinhas";
+
+    flyway.migrate();
   }
 
   @Test
