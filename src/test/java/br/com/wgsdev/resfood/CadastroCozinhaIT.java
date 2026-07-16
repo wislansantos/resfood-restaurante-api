4a53a5a5ac8f4f232e9d1310e62a95103f4102b3
Wislan Santos <wislansantosdev@gmail.com>
2026-07-16 15:18:32 -0300

    Validar o corpo da resposta HTTP (#11)

--- src/test/java/br/com/wgsdev/resfood/CadastroCozinhaIT.java
+++ src/test/java/br/com/wgsdev/resfood/CadastroCozinhaIT.java
@@ -1,11 +1,10 @@
 package br.com.wgsdev.resfood;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
-
-import static io.restassured.RestAssured.given;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -20,7 +19,7 @@ class CadastroCozinhaIT {
   public void deveRetornarStatus200_QuandoConsultarCozinhas() {
     RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 
-    given()
+    RestAssured.given()
         .basePath("/cozinhas")
         .port(port)
         .accept(ContentType.JSON)
@@ -28,5 +27,23 @@ class CadastroCozinhaIT {
         .get()
         .then()
         .statusCode(HttpStatus.OK.value());
+
   }
+
+  @Test
+  public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+    RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+    RestAssured.given()
+        .basePath("/cozinhas")
+        .port(port)
+        .accept(ContentType.JSON)
+        .when()
+        .get()
+        .then()
+        .body("", Matchers.hasSize(4))
+        .body("nome", Matchers.hasItems("Indiana", "Tailandesa"));
+
+  }
+
 }
