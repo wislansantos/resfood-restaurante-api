f3d4f42c08e9550fc892f11b6a7424566577a525
Wislan Santos <wislansantosdev@gmail.com>
2026-07-16 21:27:34 -0300

    Configurar um banco de testes e usar @ActiveProfiles no lugar de  @TestPropertySourcets (#11)

--- src/test/java/br/com/wgsdev/resfood/CadastroCozinhaIT.java
+++ src/test/java/br/com/wgsdev/resfood/CadastroCozinhaIT.java
@@ -8,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CadastroCozinhaIT {
 
