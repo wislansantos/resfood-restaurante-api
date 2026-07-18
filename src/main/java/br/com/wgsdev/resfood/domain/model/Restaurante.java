0fdffd9188a475ce3dd24d42885d75f122cef33e
Wislan Santos <wislansantosdev@gmail.com>
2026-07-18 09:19:46 -0300

    Refinar o payload de cadastro com @JsonIgnoreProperties (#12)

--- src/main/java/br/com/wgsdev/resfood/domain/model/Restaurante.java
+++ src/main/java/br/com/wgsdev/resfood/domain/model/Restaurante.java
@@ -24,6 +24,7 @@ import jakarta.validation.groups.ConvertGroup;
 import jakarta.validation.groups.Default;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -38,50 +39,49 @@ import br.com.wgsdev.resfood.core.validation.Groups;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Restaurante {
 
-	@Id
-	@EqualsAndHashCode.Include
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@NotBlank
-	@Column(nullable = false)
-	private String nome;
-	
-	@NotNull
-	@PositiveOrZero
-	@Column(name = "taxa_frete", nullable = false)
-	private BigDecimal taxaFrete;
-	
-	@Valid
-	@NotNull
-	@ConvertGroup(from = Default.class, to = Groups.CozinhaId.class)
-	@ManyToOne
-	@JoinColumn(name = "cozinha_id", nullable = false)
-	private Cozinha cozinha;
-	
-	@JsonIgnore
-	@Embedded
-	private Endereco endereco;
-	
-	@JsonIgnore
-	@CreationTimestamp
-	@Column(nullable = false, columnDefinition = "datetime")
-	private LocalDateTime dataCadastro;
-	
-	@JsonIgnore
-	@UpdateTimestamp
-	@Column(nullable = false, columnDefinition = "datetime")
-	private LocalDateTime dataAtualizacao;
-	
-	@JsonIgnore
-	@ManyToMany
-	@JoinTable(name = "restaurante_forma_pagamento",
-	    joinColumns = @JoinColumn(name = "restaurante_id"),
-	    inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
-	private List<FormaPagamento> formasPagamento = new ArrayList<>();
-	
-	@JsonIgnore
-	@OneToMany(mappedBy = "restaurante")
-	private List<Produto> produtos = new ArrayList<>();
-	
+  @Id
+  @EqualsAndHashCode.Include
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotBlank
+  @Column(nullable = false)
+  private String nome;
+
+  @NotNull
+  @PositiveOrZero
+  @Column(name = "taxa_frete", nullable = false)
+  private BigDecimal taxaFrete;
+
+  @JsonIgnoreProperties(value = "nome", allowGetters = true)
+  @Valid
+  @NotNull
+  @ConvertGroup(from = Default.class, to = Groups.CozinhaId.class)
+  @ManyToOne
+  @JoinColumn(name = "cozinha_id", nullable = false)
+  private Cozinha cozinha;
+
+  @JsonIgnore
+  @Embedded
+  private Endereco endereco;
+
+  @JsonIgnore
+  @CreationTimestamp
+  @Column(nullable = false, columnDefinition = "datetime")
+  private LocalDateTime dataCadastro;
+
+  @JsonIgnore
+  @UpdateTimestamp
+  @Column(nullable = false, columnDefinition = "datetime")
+  private LocalDateTime dataAtualizacao;
+
+  @JsonIgnore
+  @ManyToMany
+  @JoinTable(name = "restaurante_forma_pagamento", joinColumns = @JoinColumn(name = "restaurante_id"), inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+  private List<FormaPagamento> formasPagamento = new ArrayList<>();
+
+  @JsonIgnore
+  @OneToMany(mappedBy = "restaurante")
+  private List<Produto> produtos = new ArrayList<>();
+
 }
