0b772759c77a3f68897847dc2e37b139181994bc
Wislan Santos <wislansantosdev@gmail.com>
2026-07-24 14:07:53 -0300

    Adicionar endereço no modelo da representação do recurso de restaurante (#13)

---
+++ src/main/java/br/com/wgsdev/resfood/api/model/CidadeResumoModel.java
@@ -0,0 +1,14 @@
+package br.com.wgsdev.resfood.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CidadeResumoModel {
+  
+  private Long id;
+  private String nome;
+  private String estado;
+  
+}
--- src/main/java/br/com/wgsdev/resfood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/br/com/wgsdev/resfood/core/modelmapper/ModelMapperConfig.java
@@ -1,6 +1,10 @@
 package br.com.wgsdev.resfood.core.modelmapper;
 
 import org.springframework.context.annotation.Configuration;
+
+import br.com.wgsdev.resfood.api.model.EnderecoModel;
+import br.com.wgsdev.resfood.domain.model.Endereco;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 
@@ -9,7 +13,14 @@ public class ModelMapperConfig {
 
   @Bean
   public ModelMapper modelMapper() {
-    return new ModelMapper();
+    ModelMapper modelMapper = new ModelMapper();
+
+    var enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap(Endereco.class, EnderecoModel.class);
+    enderecoToEnderecoModelTypeMap.<String>addMapping(
+        enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
+        (enderecoModelDest, value) -> enderecoModelDest.getCidade().setEstado(value));
+
+    return modelMapper;
   }
 
 }
