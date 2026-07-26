98b5aba13b456d53819acedddd3680697651a6f6
Wislan Santos <wislansantosdev@gmail.com>
2026-07-26 16:03:29 -0300

    Implementar regra de negócio para evitar usuários com e-mails duplicados (#13)

--- src/main/java/br/com/wgsdev/resfood/domain/repository/UsuarioRepository.java
+++ src/main/java/br/com/wgsdev/resfood/domain/repository/UsuarioRepository.java
@@ -1,10 +1,14 @@
 package br.com.wgsdev.resfood.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Repository;
 
 import br.com.wgsdev.resfood.domain.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends CustomJpaRepository<Usuario, Long> {
+
+  Optional<Usuario> findByEmail(String email);
 
 }
