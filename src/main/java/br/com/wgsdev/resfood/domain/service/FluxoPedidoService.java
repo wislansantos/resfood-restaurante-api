3c13973f316f2ddb81eb8605fb5df5b41ce3b042
Wislan Santos <wislansantosdev@gmail.com>
2026-08-06 13:04:19 -0300

    Implementar endpoint de transição de status de pedidos (#13)

---
+++ src/main/java/br/com/wgsdev/resfood/domain/service/FluxoPedidoService.java
@@ -0,0 +1,34 @@
+package br.com.wgsdev.resfood.domain.service;
+
+import java.time.OffsetDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.wgsdev.resfood.domain.exception.NegocioException;
+import br.com.wgsdev.resfood.domain.model.Pedido;
+import br.com.wgsdev.resfood.domain.model.StatusPedido;
+
+@Service
+public class FluxoPedidoService {
+
+	@Autowired
+	private EmissaoPedidoService emissaoPedido;
+	
+	@Transactional
+	public void confirmar(Long pedidoId) {
+		Pedido pedido = emissaoPedido.buscarOuFalhar(pedidoId);
+		
+		if (!pedido.getStatus().equals(StatusPedido.CRIADO)) {
+			throw new NegocioException(
+					String.format("Status do pedido %d não pode ser alterado de %s para %s",
+							pedido.getId(), pedido.getStatus().getDescricao(), 
+							StatusPedido.CONFIRMADO.getDescricao()));
+		}
+		
+		pedido.setStatus(StatusPedido.CONFIRMADO);
+		pedido.setDataConfirmacao(OffsetDateTime.now());
+	}
+	
+}
